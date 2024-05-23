beab4332942dfff8d55c87bb703117ec8611b2c0
Abraham Paul <abrahamise.rymec@gmail.com>
2024-05-23 15:41:49 +0530

    deploy

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ const PORT = process.env.PORT || 8081
 app.listen(PORT, ()=> {
   console.log("Server is running");
 })
-
 // app.listen(8082, () => {
 //   console.log('Server is running on http://localhost:8082');
 // });
